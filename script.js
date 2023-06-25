e531741e816528456f99df4335e2040977ca9a6a
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2023-06-25 09:12:40 +0530

    Created getWeather function that takes location as console the information of current weather

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("I am working fine");
+async function getWeather(location) {
+  const api_key = "0322bd018acc44599f951507232406";
+  const response = await fetch(
+    `https://api.weatherapi.com/v1/current.json?key=${api_key}&q=${location}`,
+    { mode: "cors" }
+  );
+  const weather = await response.json();
+
+  console.log(weather);
+}
+
+getWeather("kolkata");
