8b97473f9a202a39c1c6a9bd640c99b4cc34d276
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2023-06-30 19:39:46 +0530

    Created weather function that displays the current weather data on screen

--- script.js
+++ script.js
@@ -6,10 +6,31 @@ async function getCurrentWeather(location) {
       { mode: "cors" }
     );
     const currentWeatherInfo = await response.json();
-    console.log(currentWeatherInfo);
+    weather(currentWeatherInfo);
   } catch (error) {
     console.error(error);
   }
 }
 
 getCurrentWeather("kolkata");
+
+let weather = (currentWeatherInfo) => {
+  const condition = document.querySelector(".condition");
+  const location = document.querySelector(".location");
+  const dateTime = document.querySelector(".date-time");
+  const temperature = document.querySelector(".temperature");
+
+  condition.textContent = currentWeatherInfo.current.condition.text;
+  location.textContent = currentWeatherInfo.location.name;
+  dateTime.textContent = currentWeatherInfo.location.localtime;
+
+  const img = document.createElement("img");
+  img.src = currentWeatherInfo.current.condition.icon;
+  temperature.appendChild(img);
+
+  const temp = document.createElement("div");
+  temp.innerHTML = `${currentWeatherInfo.current.temp_c} ℃`;
+  temperature.appendChild(temp);
+
+  console.log(currentWeatherInfo);
+};
